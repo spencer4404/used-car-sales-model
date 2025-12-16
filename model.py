741235fa7bcce98c096b8ad74df726fb1298bcc0
Spencer Iannantuono <133061786+spencer4404@users.noreply.github.com>
2025-12-16 17:50:54 -0500

    created up to the pipline

--- model.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("sandeep1080/used-car-sales")
-
-df = pd.read_csv(os.path.join(path, "used_car_sales.csv"))
